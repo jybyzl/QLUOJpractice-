5dc78a236ebf2a8fee14c06c2c99b17f01cd54f0
jybyzl <111415517+jybyzl@users.noreply.github.com>
2023-01-10 01:02:55 +0800

    Add files via upload

--- QLUOJ1001.c
+++ QLUOJ1001.c
@@ -2,16 +2,33 @@
 
 int main(int argc,const char*argv[]){
 
+    // int n;
+    // while ( scanf("%d",&n) != EOF )
+    // {
+    //     int sum;
+    //     int i;
+    //     for ( i = 1; i <= n; i++ ){
+    //         sum += i;
+    //     }
+    //     printf("%d\n", sum);
+    //     sum = 0;
+    // }
+    // return 0;
+
     int n;
-    while ( scanf("%d",&n) != EOF )
+    while (scanf("%d",&n) != EOF)
     {
-        int sum;
-        int i;
-        for ( i = 1; i <= n; i++ ){
-            sum += i;
-        }
-        printf("%d\n", sum);
-        sum = 0;
+        printf("%d\n", sum(n));
     }
     return 0;
+}
+
+int sum(int n){
+
+
+    if( n == 1 ){
+        return 1;
+    }else{
+        return sum(n - 1) + n;
+    }
 }
